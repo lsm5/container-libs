0689782961e947ee9af648d4eb5934c6f1666eeb
Lokesh Mandvekar <lsm5@redhat.com>
2025-11-06 13:17:21 -0500

    cgroups.AvailableControllers simplification for cgv2 only
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- common/pkg/cgroups/cgroups_linux.go
+++ common/pkg/cgroups/cgroups_linux.go
@@ -92,63 +92,37 @@ func init() {
 }
 
 // getAvailableControllers get the available controllers.
-func getAvailableControllers(exclude map[string]controllerHandler, cgroup2 bool) ([]controller, error) {
-	if cgroup2 {
-		controllers := []controller{}
-		controllersFile := filepath.Join(cgroupRoot, "cgroup.controllers")
-
-		// rootless cgroupv2: check available controllers for current user, systemd or servicescope will inherit
-		if unshare.IsRootless() {
-			userSlice, err := getCgroupPathForCurrentProcess()
-			if err != nil {
-				return controllers, err
-			}
-			// userSlice already contains '/' so not adding here
-			basePath := cgroupRoot + userSlice
-			controllersFile = filepath.Join(basePath, "cgroup.controllers")
-		}
-		controllersFileBytes, err := os.ReadFile(controllersFile)
-		if err != nil {
-			return nil, fmt.Errorf("failed while reading controllers for cgroup v2: %w", err)
-		}
-		for controllerName := range strings.FieldsSeq(string(controllersFileBytes)) {
-			c := controller{
-				name:    controllerName,
-				symlink: false,
-			}
-			controllers = append(controllers, c)
-		}
-		return controllers, nil
-	}
-
-	subsystems, _ := cgroupV1GetAllSubsystems()
+func getAvailableControllers() ([]controller, error) {
 	controllers := []controller{}
-	// cgroupv1 and rootless: No subsystem is available: delegation is unsafe.
-	if unshare.IsRootless() {
-		return controllers, nil
-	}
+	controllersFile := filepath.Join(cgroupRoot, "cgroup.controllers")
 
-	for _, name := range subsystems {
-		if _, found := exclude[name]; found {
-			continue
-		}
-		fileInfo, err := os.Stat(cgroupRoot + "/" + name)
+	// rootless cgroupv2: check available controllers for current user, systemd or servicescope will inherit
+	if unshare.IsRootless() {
+		userSlice, err := getCgroupPathForCurrentProcess()
 		if err != nil {
-			continue
+			return controllers, err
 		}
+		// userSlice already contains '/' so not adding here
+		basePath := cgroupRoot + userSlice
+		controllersFile = filepath.Join(basePath, "cgroup.controllers")
+	}
+	controllersFileBytes, err := os.ReadFile(controllersFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed while reading controllers for cgroup v2: %w", err)
+	}
+	for controllerName := range strings.FieldsSeq(string(controllersFileBytes)) {
 		c := controller{
-			name:    name,
-			symlink: !fileInfo.IsDir(),
+			name:    controllerName,
+			symlink: false,
 		}
 		controllers = append(controllers, c)
 	}
-
 	return controllers, nil
 }
 
 // AvailableControllers get string:bool map of all the available controllers.
-func AvailableControllers(exclude map[string]controllerHandler, cgroup2 bool) ([]string, error) {
-	availableControllers, err := getAvailableControllers(exclude, cgroup2)
+func AvailableControllers() ([]string, error) {
+	availableControllers, err := getAvailableControllers()
 	if err != nil {
 		return nil, err
 	}
@@ -158,31 +132,6 @@ func AvailableControllers(exclude map[string]controllerHandler, cgroup2 bool) ([
 	}
 
 	return controllerList, nil
-}
-
-func cgroupV1GetAllSubsystems() ([]string, error) {
-	f, err := os.Open("/proc/cgroups")
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	subsystems := []string{}
-
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		text := s.Text()
-		if text[0] != '#' {
-			parts := strings.Fields(text)
-			if len(parts) >= 4 && parts[3] != "0" {
-				subsystems = append(subsystems, parts[0])
-			}
-		}
-	}
-	if err := s.Err(); err != nil {
-		return nil, err
-	}
-	return subsystems, nil
 }
 
 func getCgroupPathForCurrentProcess() (string, error) {
@@ -309,14 +258,6 @@ func New(path string, resources *cgroups.Resources) (*CgroupControl, error) {
 		},
 	}
 
-	if !cgroup2 {
-		controllers, err := getAvailableControllers(handlers, false)
-		if err != nil {
-			return nil, err
-		}
-		control.additionalControllers = controllers
-	}
-
 	if err := control.initialize(); err != nil {
 		return nil, err
 	}
@@ -355,13 +296,6 @@ func Load(path string) (*CgroupControl, error) {
 		config: &cgroups.Cgroup{
 			Path: path,
 		},
-	}
-	if !cgroup2 {
-		controllers, err := getAvailableControllers(handlers, false)
-		if err != nil {
-			return nil, err
-		}
-		control.additionalControllers = controllers
 	}
 	if !cgroup2 {
 		oneExists := false
